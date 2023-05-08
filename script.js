8b9e48c088c515ab6c03d009eda6dfc47155feb0
Mutayyabweb <126829771+Mutayyabweb@users.noreply.github.com>
2023-05-08 12:40:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// dataTypes
+// there are to types of data types
+// permetive
+// string
+// Nuymber
+// null 
+// undefined
+// bigint
+// symbol
+// boolean
+// let h1 = 23;
+// let h2="heloo"
+// let h3=null
+// let h4;
+// let id=Symbol("h1")
+// let h5=BigInt(542)
+// let h6 = true;
+// console.table([typeof h1,typeof h2,typeof h3, typeof h4,typeof id,typeof h5, typeof h6,h1,h2,h3,h4,id,h5,h6])
+// non(permmetive)
+
+// const arr=["email","name","class",210]
+// console.log(arr);
+// console.log(typeof arr);
+
+
+// const h2={email:"mutayyab",
+// number:25}
+// console.log(h2);
+// console.log(typeof h2);
+
+
+
+
+
+// STRINGS AND THERE METHODS
+// let str="                   Mutayyab                      "
+// let str1="hello kase ho ap" 
+// 1.console.log(str.replace("m","n"));//It is use to replace 
+// 2.console.log(str1.split("o")); //It is use to split
+// 3.console.log(str.length); //It is use to find length
+// 4.console.log(str.toLowerCase()); //It is use to convert string in lowerCase
+// 5.console.log(str.match("a"));//It is use to match the argument is available are not
+// 6.console.log(str.charAt(0)); //It is use to find character at given index or not
+// 7.console.log(str.charCodeAt("b")); //It is use to find alphabatical code
+// 8.console.log(str.codePointAt(str1));
+// 9.console.log(str.concat(str1));
+// 10.console.log(str.endsWith("a"));`
+// 11.console.log(str.includes("g"));
+// 12.console.log(str.indexOf("y"));
+// 13.console.log(str.lastIndexOf("a"));
+// 14.console.log(str.localeCompare("M"));
+// 15.console.log(str.slice(2,7));
+// 16.console.log(str.trim());
+// 17.console.log(str.valueOf());
+// console.log(str);
