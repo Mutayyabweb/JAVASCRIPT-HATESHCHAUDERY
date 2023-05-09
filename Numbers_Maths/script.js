0e347d1eeb0b53283c5a87294cef6837d801f67d
Mutayyabweb <126829771+Mutayyabweb@users.noreply.github.com>
2023-05-09 09:45:45 +0100

    Maths_Numbers

---
+++ Numbers_Maths/script.js
@@ -0,0 +1,26 @@
+// Numbers
+// const number =1000000;
+// console.log(number.toFixed(2)); //It give Numbers
+// console.log(number.toPrecision(4));
+// console.log(number.toExponential(3));
+// console.log(number.toLocaleString("en-pk"));
+// console.log(number.toString(2));
+// console.log(number.valueOf());
+
+
+// Maths
+
+
+// console.log(Math.abs(-4));//It converts negative value to postive
+// console.log(Math.round(4.4));//if value is less than 4.5 it converts 4 and if value is greater than 4.5 or 4.5 it rounded it on 5;
+// console.log(Math.ceil(4.1));//it rounded value to greater number 4.1 it rounded to 5
+// console.log(Math.floor(4.9));//it is opposite of celi it gives value in lower number 4.9 it gives 4
+// let m=Math.random()+10;
+// // console.log(Math.floor(m));
+// const min=20;
+// const max=30;
+// console.log(Math.floor(Math.random()*(max-min +1))+min)
+
+let min=5;
+let max=10;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
